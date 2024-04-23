65a33d6c1759e160f11d22a32e910404804e384b
Tenley <166721312+t3nl3y@users.noreply.github.com>
2024-04-23 09:24:07 -0400

    ed

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 function tester(month, day) {
     //alert(month + ' ' + day);
     console.log("in the console");
-    document.getElementById("calendarid").innerHTML = month + ", " + day;
+    document.getElementById("calendarid").innerHTML = "calendar";
 }
