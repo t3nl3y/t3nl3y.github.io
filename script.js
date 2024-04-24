a1b33a5593aef13459d4e8288774b1f39b998d53
Tenley <166721312+t3nl3y@users.noreply.github.com>
2024-04-24 09:02:15 -0400

    edit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
 function tester(month, day) {
-    document.getElementById("calenderid").innerHTML = month + '' + day;
+    document.getElementById("calenderid").innerHTML = month + ' ' + day;
     //button to go back / restore previos innerHTML to see calendar again
 }
