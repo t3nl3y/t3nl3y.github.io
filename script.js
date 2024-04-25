b28cca0fa6e248ab3921932fc1e5be0733e456e2
Tenley <166721312+t3nl3y@users.noreply.github.com>
2024-04-25 09:41:24 -0400

    edit

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function tester(month, day) {
     document.getElementById("calenderid").innerHTML = month + ' ' + day;
     //button to go back / restore previos innerHTML to see calendar again
 }
+
+function add_event() {
+    userinput = prompt("Title: ")
+
+
+}
