50f6d8777a9558edb58dc39cd6db75cd1b9649de
Tenley <166721312+t3nl3y@users.noreply.github.com>
2024-05-03 09:08:20 -0400

    update

--- script.js
+++ script.js
@@ -10,33 +10,33 @@ function add_event() {
     color = prompt("Color: ")
     var div = document.createElement("div");
     if (color == "red") {
-        div.style.color = #f11e1e
+        div.style.color = "#f11e1e"; 
     }
     if (color == "orange") {
-        div.style.color = #ff8119
+        div.style.color = "#ff8119";
     }
     if (color == "yellow") {
-        div.style.color = #a89009
+        div.style.color = "#a89009";
     }
     if (color == "light green") {
-        div.style.color = #5cb904
+        div.style.color = "#5cb904";
     }
     if (color == "dark green") {
-        div.style.color = #037a1d
+        div.style.color = "#037a1d";
     }
     if (color == "blue") {
-        div.style.color = #2683ec
+        div.style.color = "#2683ec";
     }
     if (color == "navy") {
-        div.style.color = #031f7a
+        div.style.color = "#031f7a";
     }
     if (color == "purple") {
-        div.style.color = #a155ce
+        div.style.color = "#a155ce";
     }
     if (color == "pink") {
-        div.style.color = #bb1692
+        div.style.color = "#bb1692";
     }
     if (color == "black") {
-        div.style.color = #000000
+        div.style.color = "#000000";
     }
 } 
