15d44c3a3700591c6131049f1f339e0b73aa50f2
Tenley <166721312+t3nl3y@users.noreply.github.com>
2024-05-07 09:03:56 -0400

    edits

--- script.js
+++ script.js
@@ -42,3 +42,38 @@ function add_event() {
         div.style.color = "#000000";
     }
 }
+function add_alldayevent() {
+    userinput = prompt("Title: ")
+    color = prompt("Color: ") 
+    document.getElementById(eventallday).innerHTML += userinput;
+    if (color == "red") {
+        div.style.color = "#f11e1e"; 
+    }
+    if (color == "orange") {
+        div.style.color = "#ff8119";
+    }
+    if (color == "yellow") {
+        div.style.color = "#a89009";
+    }
+    if (color == "light green") {
+        div.style.color = "#5cb904";
+    }
+    if (color == "dark green") {
+        div.style.color = "#037a1d";
+    }
+    if (color == "blue") {
+        div.style.color = "#2683ec";
+    }
+    if (color == "navy") {
+        div.style.color = "#031f7a";
+    }
+    if (color == "purple") {
+        div.style.color = "#a155ce";
+    }
+    if (color == "pink") {
+        div.style.color = "#bb1692";
+    }
+    if (color == "black") {
+        div.style.color = "#000000";
+    }
+}
