7204ab7e6a530c0f52e1cecf7a8d9cdd8febfd63
Tenley <166721312+t3nl3y@users.noreply.github.com>
2024-05-17 09:28:35 -0400

    deit

--- script.js
+++ script.js
@@ -48,32 +48,32 @@ function add_alldayevent() {
     color = prompt("Color: ") 
     document.getElementById("event_allday").innerHTML += "<br>" + userinput;
     if (color == "red") {
-        div.style.color = "#f11e1e"; 
-    }
+        div.style.color = "#f11e1e";
+        }
     if (color == "orange") {
         div.style.color = "#ff8119";
-    }
+        }
     if (color == "yellow") {
         div.style.color = "#a89009";
-    }
+        }
     if (color == "light green") {
         div.style.color = "#5cb904";
-    }
+        }
     if (color == "dark green") {
         div.style.color = "#037a1d";
-    }
+        }
     if (color == "blue") {
         div.style.color = "#2683ec";
-    }
+        }
     if (color == "navy") {
         div.style.color = "#031f7a";
-    }
+        }
     if (color == "purple") {
         div.style.color = "#a155ce";
-    }
+        }
     if (color == "pink") {
         div.style.color = "#bb1692";
-    }
+        }
     if (color == "black") {
         div.style.color = "#000000";
     } 
